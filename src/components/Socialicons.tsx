c94cbee96aa6a6b42fac9c5cdc39e5747684cb5f
nigam <nigamsharma23112001@gmail.com>
2023-09-22 19:44:55 +0530

    homepage

--- src/components/Socialicons.tsx
+++ src/components/Socialicons.tsx
@@ -1,13 +1,50 @@
-import React from 'react'
-import{
-
-} from "react-icons/fi"
+import Link from "next/link";
+import React from "react";
+import {
+  FiGithub,
+  FiInstagram,
+  FiYoutube,
+  FiLinkedin,
+  FiTwitter,
+} from "react-icons/fi";
 function Socialicons() {
+  const socialLinks = [
+    { name: "Github", icon: <FiGithub />, link: "https://www.github.com" },
+    {
+      name: "Youtube",
+      icon: <FiYoutube />,
+      link: "",
+    },
+    {
+      name: "LinkedIn",
+      icon: <FiLinkedin />,
+      link: "",
+    },
+    {
+      name: "Instagram",
+      icon: <FiInstagram />,
+      link: "",
+    },
+    {
+      name: "Twitter",
+      icon: <FiTwitter />,
+      link: "",
+    },
+  ];
   return (
-    <div>
-      
+    <div className="social-icons">
+      <ul className="social-icons-list">
+        {socialLinks.map(({ name, icon, link }) => (
+          <li key={name} title={name} className="social-icons-list-item">
+            <Link href={link} className="social-icons-list-item-link" target="_blank">
+{icon}
+
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Socialicons
+export default Socialicons;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Socialicons from '@/components/Socialicons'
 import Navbar from '@/sections/Navbar'
 import React from 'react'
 
@@ -5,6 +6,7 @@ function Index() {
   return (
     <div className='App'>
       <Navbar/>
+      <Socialicons/>
     </div>
   )
 }
